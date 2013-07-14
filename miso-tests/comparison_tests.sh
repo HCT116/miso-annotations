90fa135ff70cde3f3dd5eee30bb238e64fa1cc2f
Yarden Katz <yarden.katz@gmail.com>
2013-07-14 01:09:23 -0400

    revs

--- miso-tests/comparison_tests.sh
+++ miso-tests/comparison_tests.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 compare_miso --compare-samples ./miso_output/KH2_NoDox ./miso_output/KH2_DOX ./comparisons
+
+#filter_events --filter 
--- miso-tests/summary_tests.sh
+++ miso-tests/summary_tests.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Summarizing samples..."
-summarize_miso --summarize-samples ./miso_output/KH2_NoDox ./miso_output/KH2_NoDox/summary/
-summarize_miso --summarize-samples ./miso_output/KH2_DOX ./miso_output/KH2_DOX/summary/
+
+#summarize_miso --summarize-samples ./miso_output/KH2_NoDox ./miso_output/KH2_NoDox/
+
+summarize_miso --summarize-samples ./miso_output/KH2_DOX ./miso_output/KH2_DOX/
+
