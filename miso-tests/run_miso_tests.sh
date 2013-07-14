a8f8461c3bf70f5ee6ecf8aaf70a69eb864f7bf8
Yarden Katz <yarden.katz@gmail.com>
2013-07-14 10:02:43 -0400

    Adding prefilter option

--- miso-tests/run_miso_tests.sh
+++ miso-tests/run_miso_tests.sh
@@ -7,6 +7,6 @@ settings=~/jaen/Musashi/rna-seq/settings/miso_settings.txt
 echo "Testing KH2_NoDox on cluster"
 miso --run $SE ./bams/KH2_NoDox.combined.sorted.bam --output-dir ./miso_output/KH2_NoDox --read-len $rl --use-cluster --chunk-jobs 200 --settings-filename $settings
 
-# Run second sample multi-threaded
+# Run second sample multi-threaded: use --prefilter option
 echo "Testing KH2_DOX locally"
-miso --run $SE ./bams/KH2_DOX.combined.sorted.bam --output-dir ./miso_output/KH2_DOX --read-len $rl -p 5 --settings-filename $settings
+miso --run $SE ./bams/KH2_DOX.combined.sorted.bam --prefilter --output-dir ./miso_output/KH2_DOX --read-len $rl -p 5 --settings-filename $settings
